0918a99b837c130ac6f36406372edc81f68a1402
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-08-30 16:18:23 +0200

    Startpage

--- foretagssida-webshop/src/components/Startpage.js
+++ foretagssida-webshop/src/components/Startpage.js
@@ -4,7 +4,11 @@ function Startpage(){
     return (
         <main className='wrapper'>
             <div className='gray-area'>
-                
+                <div className='inner-contents'>
+                    <h1>
+                    Something magical will happen when you read a good book.
+                    </h1>
+                </div>
             </div>
             <div className='white-area'>
 
