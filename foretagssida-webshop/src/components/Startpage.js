0031463e899bf2c80a927b3822bc9d44c71e86c2
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-08-30 16:04:30 +0200

    Setting up routes + startpage

---
+++ foretagssida-webshop/src/components/Startpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Startpage(){
+    return (
+        <main className='wrapper'>
+            <div className='gray-area'>
+                
+            </div>
+            <div className='white-area'>
+
+            </div>
+        </main>
+    )
+}
+export default Startpage
