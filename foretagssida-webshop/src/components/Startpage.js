4bf562e8e78b5a6b98782428d601f8e7c32b99c4
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-08-31 10:55:22 +0200

    Hero done

--- foretagssida-webshop/src/components/Startpage.js
+++ foretagssida-webshop/src/components/Startpage.js
@@ -5,13 +5,22 @@ function Startpage(){
         <main className='wrapper'>
             <div className='hero'>
                 <div className='hero-contents'>
-                    <h1 className="hero-title">
-                    Something magical will happen when you read a good book.
-                    </h1>
-                    <p className="hero-text">
-                    Bookplace helps you keep track of your reading,
-                    & meet some new friends along the way.
-                    </p>
+                    <div className="hero-box hero-box-1">
+                        <h1 className="hero-title">
+                        Something magical will happen when you read a good book.
+                        </h1>
+                    </div>
+                    <div className="hero-box hero-box-2">
+                        <p className="hero-text">
+                        Bookplace helps you keep track of your reading,
+                        & meet some new friends along the way.
+                        </p>
+                    </div>
+                    <div className="hero-box hero-box-3">
+                        <button className="btn">
+                            Get started
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className='white-area'>
