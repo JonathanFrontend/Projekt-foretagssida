543cd6fa6abd3be99f8eeb7b15360a99cebad221
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-08-31 09:39:32 +0200

    Startpage html css

--- foretagssida-webshop/src/components/Startpage.js
+++ foretagssida-webshop/src/components/Startpage.js
@@ -3,11 +3,15 @@ import React from 'react';
 function Startpage(){
     return (
         <main className='wrapper'>
-            <div className='gray-area'>
-                <div className='inner-contents'>
-                    <h1>
+            <div className='hero'>
+                <div className='hero-contents'>
+                    <h1 className="hero-title">
                     Something magical will happen when you read a good book.
                     </h1>
+                    <p className="hero-text">
+                    Bookplace helps you keep track of your reading,
+                    & meet some new friends along the way.
+                    </p>
                 </div>
             </div>
             <div className='white-area'>
