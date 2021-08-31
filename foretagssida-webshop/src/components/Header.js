76d86ce547febaa77404d6d3d761c2199a24ea8c
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2021-08-31 15:31:56 +0200

    Update SV/EN
    Fixat SV till EN, statiskt

--- foretagssida-webshop/src/components/Header.js
+++ foretagssida-webshop/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../components/Vector.png'
 
@@ -6,33 +6,58 @@ import arrowDown from '../components/arrow-down.svg'
 
 
 
-
-
 function Header() {
+
+    let [meny, setMeny] = useState(true);
+
+
+    console.log(meny)
     return (
         <nav className="nav_bar">
 
-
-            <div className="nav_company">
-                <img className="nav_logo" src={logo} alt="company_logo" />
-                <h3 className="nav_head">Bookstore</h3>
-            </div>
+            <Link className="link_head" to="/">
+                <div className="nav_company">
+                    <img className="nav_logo" src={logo} alt="company_logo" />
+                    <h3 className="nav_head">Bookstore</h3>
+                </div>
+            </Link>
 
             <section className="nav_menu">
                 <ul>
-                    <Link className="nav_link">Om oss</Link>
-                    <Link className="nav_link">Vanliga frågor</Link>
-                    <Link className="nav_link">Kontakt</Link>
+                    <Link to="/about" className="nav_link">Om oss</Link>
+                    <Link to="/faq" className="nav_link">Vanliga frågor</Link>
+                    <Link to="/contact" className="nav_link">Kontakt</Link>
                 </ul>
             </section>
 
 
             <section className="nav_buttons">
-                <p className="first_btn">SV
-                    <button className="arrow_down">
-                        <img className="arrow_size" src={arrowDown} alt="arrow_down" />
-                    </button>
-                </p>
+                {meny ?
+                    <div className="one">
+                        <p className="first_btn">SV</p>
+                        <button onClick={() => setMeny(!meny)} className="arrow_down">
+                            <img className="arrow_size" src={arrowDown} alt="arrow_down" />
+                        </button>
+                    </div>
+                    :
+                    <section>
+                        <div className="one">
+                            <p className="first_btn">SV</p>
+                            <button onClick={() => setMeny(!meny)} className="arrow_down">
+                                <img className="arrow_size" src={arrowDown} alt="arrow_down" />
+                            </button>
+                        </div>
+
+                        <button className="hidden two">EN</button>
+
+
+                    </section>
+                }
+
+
+
+
+
 
             </section>
 
