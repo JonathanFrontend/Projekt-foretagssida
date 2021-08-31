76adbca241513b2d8aac8e7794a256df4e02ff8a
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-08-31 10:12:29 +0200

    Update About.js

--- foretagssida-webshop/src/components/About.js
+++ foretagssida-webshop/src/components/About.js
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import { useEffect } from 'react';
+import personaljson from ""
 
 function About() {
+
+
+
+    useEffect(() =>{
+
+        async function fetchData() {
+        const response = await fetch(personaljson);
+        const data = await response.json();
+        console.log(data);
+       
+        }
+        fetchData();
+    
+    },[])
+
+
     return (
         <div>
-            
+            <h1>hej</h1>
         </div>
     )
 }
