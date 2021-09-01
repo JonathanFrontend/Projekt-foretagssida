dfbe49f2dc4b89d26417b452eaf8bc837338437c
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-09-01 11:39:53 +0200

    update new picture i css

--- foretagssida-webshop/src/components/About.js
+++ foretagssida-webshop/src/components/About.js
@@ -12,6 +12,9 @@ function About() {
                     <h3 className="about-h3">Företagsmotto</h3>
                     <h2 className="about-h2"> När du läser en bra bok då händer alltid nåt magiskt!</h2> 
                     <p className="about-p">Vi erbjuder klassiska böcker, tidsskrifter och ljudböcker till ett förmånligt pris.</p> 
+                    <div className="about-img">
+                        <h1 className="about-img-text">NO LIMIT</h1>
+                    </div>
                     <h3 className="about-h3">Medarbetare</h3>
                 <div className="personal-content">
                     {Staff.personal.map((staff, key) => <div className="personal-card" key={key}> 
