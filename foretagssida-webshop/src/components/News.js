32f8f19e5883f06928c4f1be701bb2628d7def42
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2021-09-02 12:18:30 +0200

    update news

--- foretagssida-webshop/src/components/News.js
+++ foretagssida-webshop/src/components/News.js
@@ -10,6 +10,44 @@ export default function News() {
 
     let [addFilter, setAddFilter] = useState(false);
     let [addSort, setAddSort] = useState(false);
+    let [content, setContent] = useState(articles)
+
+    const [checked, setChecked] = useState(false);
+
+
+    function newsClick() {
+        console.log(checked)
+        if (checked === true) {
+            setContent(articles)
+
+        } else if (checked === false) {
+            console.log('sant itryckt')
+            const news = articles.filter(article => article.label === 'news')
+            setContent(news)
+
+        } else {
+            console.log('nada')
+        }
+    }
+    function artClick() {
+        console.log(checked)
+        if (checked === true) {
+            console.log('inte itryckt')
+            setContent(articles)
+
+        } else if (checked === false) {
+            console.log('sant itryckt')
+            const news = articles.filter(article => article.label === 'articles')
+            setContent(news)
+
+        } else {
+            console.log('nada')
+        }
+    }
+
+
+
+
 
     return (
         <div>
@@ -30,14 +68,14 @@ export default function News() {
                             <img className="arrow_size" src={arrowDown} alt="arrow_down" />
                         </button>
                     </div>
-                    {addFilter ?    
+                    {addFilter ?
                         <div>
                             <button>Nyheter</button>
-                            <input type="checkbox" defaultChecked />
+                            <input type="checkbox" defaultChecked={checked} onClick={newsClick} onChange={() => setChecked(!checked)} />
                             <button>Artiklar</button>
-                            <input type="checkbox" defaultChecked />
+                            <input type="checkbox" defaultChecked={checked} onClick={artClick} onChange={() => setChecked(!checked)} />
                         </div>
-                        : '' }
+                        : ''}
                 </div>
 
                 <div className="newsBoxSort">
@@ -48,28 +86,28 @@ export default function News() {
                             <img className="arrow_size" src={arrowDown} alt="arrow_down" />
                         </button>
                     </div>
-                    {addSort ? 
+                    {addSort ?
                         <div>
                             <button>Datum - Senaste Först</button>
                             <input type="radio" name="sort" defaultChecked />
                             <button>Datum - Äldst Först</button>
                             <input type="radio" name="sort" />
                         </div>
-                    : '' }
+                        : ''}
                 </div>
 
             </div>
 
             <div>
-                {articles.map((data, key) => 
-                <div key={key}>
-                    <h6>{data.headline}</h6>
-                    <div>
-                        <p>{data.date}</p>
-                        <p>Författare: {data.author}</p>
-                        <p>Inlägg: {data.label}</p>
+                {content.map((data, key) =>
+                    <div key={key}>
+                        <h6>{data.headline}</h6>
+                        <div>
+                            <p>{data.date}</p>
+                            <p>Författare: {data.author}</p>
+                            <p>Inlägg: {data.label}</p>
+                        </div>
                     </div>
-                </div>
                 )}
             </div>
 
