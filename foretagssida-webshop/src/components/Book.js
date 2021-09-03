99351cacfc13c8518402c194f03b2afb9f4382ec
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-09-03 11:37:07 +0200

    Update Book.js

--- foretagssida-webshop/src/components/Book.js
+++ foretagssida-webshop/src/components/Book.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Book(props){
-    console.log(props);
     const [wantToRead, setWantToRead] = useState(props.book.wantToRead);
     let typeOfCheck = wantToRead ? "green-check" : "black-check";
     const rating = props.book.rating;
     const title = props.book.title;
     const author = props.book.author;
     const price = props.book.price;
+    const starPercentage = (rating / 5) * 100 + '%';
+    console.log(starPercentage);
+
     return (
         <aside className="book-container" key={props.book.id}>
             <span className={"check " + typeOfCheck} onClick={() => setWantToRead(!wantToRead)}>
-            {wantToRead ? <i class="fas fa-check"></i> : <i class="fas fa-plus"></i>}
+            {wantToRead ? <i className="fas fa-check"></i> : <i className="fas fa-plus"></i>}
             </span>
             {/* <img src={`/images/${props.book.poster}`} alt="{props.book.title}" className="book-image"/> */}
             <div className="book-poster" style={{backgroundImage: `url("/images/${props.book.poster}")`}}>
 
             </div>
             {rating ? 
-            <div className="rating-box">
-                <h3>
-                    {rating} <i class="fas fa-star"></i>
-                </h3>
-            </div> : ""}
+                <div className="rating-box">
+                    <div className="star-outer">
+                        <div className="star-inner" style={{width: starPercentage}}></div>
+                    </div>
+                </div> : ""
+            }
             <div className="title-box">
                 <h3>
                     {title}
