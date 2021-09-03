43d39da51c8e313f30aede6d62df702999ea18cb
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2021-09-03 14:32:03 +0200

    update CSS
    Update CSS

--- foretagssida-webshop/src/components/News.js
+++ foretagssida-webshop/src/components/News.js
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from 'react';
 import articles from "../data/articles.json"
 
 // Import Pictures
-import arrowDown from '../components/arrow-down.svg'
+import arrowDownPurp from '../components/arrow-down-purp.png'
 
 export default function News() {
 
     let [addFilter, setAddFilter] = useState(false);
     let [addSort, setAddSort] = useState(false);
-    
+
     let [content, setContent] = useState(articles)
 
     const [checkedOne, setCheckedOne] = useState(false);
@@ -18,11 +18,11 @@ export default function News() {
 
     // This usestate and if else is for sorting our News and Articles
     const [radioChecked, setRadioChecked] = useState(true);
-    
+
     if (radioChecked) {
-        content.sort((a, b) => (parseInt(b.date)  - parseInt(a.date)))
+        content.sort((a, b) => (parseInt(b.date) - parseInt(a.date)))
     } else {
-        content.sort((a, b) => (parseInt(a.date)  - parseInt(b.date))) 
+        content.sort((a, b) => (parseInt(a.date) - parseInt(b.date)))
     }
 
     // This useeffect is handling the Filter function
@@ -62,17 +62,24 @@ export default function News() {
                         <div className="newsArrow">
                             <p>Filtrera</p>
                             <button className="arrow_down" onClick={() => setAddFilter(!addFilter)}>
-                                <img className="arrow_size" src={arrowDown} alt="arrow_down" />
+                                <img className="arrow_size purp" src={arrowDownPurp} alt="arrow_down" />
                             </button>
                         </div>
                         {addFilter ?
-                            <div>
-                                <button>Nyheter</button>
-                                <input type="checkbox" checked={checkedOne} onChange={() => setCheckedOne(!checkedOne)} />
-                                <button>Artiklar</button>
-                                <input type="checkbox" checked={checkedTwo} onChange={() => setCheckedTwo(!checkedTwo)} />
+                            <div className="newsButtonBox">
+                                <aside className="newsAside">
+                                    <button className="newsButton">Nyheter</button>
+                                    <input type="checkbox" checked={checkedOne} onChange={() => setCheckedOne(!checkedOne)} />
+                                </aside>
+
+                                <aside className="newsAside">
+                                    <button className="newsButton">Artiklar</button>
+                                    <input type="checkbox" checked={checkedTwo} onChange={() => setCheckedTwo(!checkedTwo)} />
+                                </aside>
                             </div>
+
                             : ''}
+
                     </div>
 
                     <div className="newsBoxFilter">
@@ -80,27 +87,33 @@ export default function News() {
                         <div className="newsArrow">
                             <p>Sortera</p>
                             <button className="arrow_down" onClick={() => setAddSort(!addSort)}>
-                                <img className="arrow_size" src={arrowDown} alt="arrow_down" />
+                                <img className="arrow_size" src={arrowDownPurp} alt="arrow_down" />
                             </button>
                         </div>
                         {addSort ?
-                            <div>
-                                <button>Datum - Senaste Först</button>
-                                <input type="radio" name="sort" defaultChecked  onClick={() => setRadioChecked(true)} />
-                                <button>Datum - Äldst Först</button>
-                                <input type="radio" name="sort" onClick={() => setRadioChecked(false)} />
+                            <div className="articleButtonBox">
+                                <aside className="articleAside">
+                                    <button className="articleButton">Datum - Senaste Först</button>
+                                    <input type="radio" name="sort" defaultChecked onClick={() => setRadioChecked(true)} />
+                                </aside>
+
+
+                                <aside className="articleAside">
+                                    <button className="articleButton">Datum - Äldst Först</button>
+                                    <input type="radio" name="sort" onClick={() => setRadioChecked(false)} />
+                                </aside>
                             </div>
                             : ''}
                     </div>
 
                 </div>
 
-                <div>
+                <div className="content">
                     {content.map((data, key) =>
-                        <div key={key}>
-                            <h6>{data.headline}</h6>
-                            <div>
-                                <p>             {data.date}</p>
+                        <div className="contentBox" key={key}>
+                            <h6 className="contentHeadline">{data.headline}</h6>
+                            <div className="miniContent">
+                                <p>{data.date}</p>
                                 <p>Författare: {data.author}</p>
                                 <p>Inlägg: {data.label}</p>
                             </div>
@@ -108,6 +121,6 @@ export default function News() {
                     )}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
