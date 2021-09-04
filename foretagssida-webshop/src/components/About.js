ea4dc92de755947cecea43f46f4363470f96bd19
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2021-09-04 09:54:48 +0200

    update Swedish languages

--- foretagssida-webshop/src/components/About.js
+++ foretagssida-webshop/src/components/About.js
@@ -1,28 +1,24 @@
 import {React} from 'react';
-import lang from "../lang.json"
+import personal from "../data/personal.json"
 import { useSelector, useDispatch } from 'react-redux';
 function About() {
-    
+
     let stateLang = useSelector(state => {
         return state.lang
     });
-
     let dispatch = useDispatch();
-    console.log('State Lang:', stateLang);
+    
     const text = () => {
         switch(stateLang) {
             case 'en':
-                return lang.en;
+                return personal.en;
             case 'sv':
-                return lang.sv;
+                return personal.sv;
             default:
-                return lang.sv;
+                return personal.sv;
         }
     };
-        console.log(stateLang);
-       
-
-
+        
    return (
         <div className="personal-container">
             <div className="header-img">
@@ -32,11 +28,13 @@ function About() {
                     <h3 className="about-h3">Företagsmotto</h3>
                     <h2 className="about-h2"> När du läser en bra bok då händer alltid nåt magiskt!</h2> 
                     <p className="about-p">Vi erbjuder klassiska böcker, tidsskrifter och ljudböcker till ett förmånligt pris.</p> 
-                    <div className="about-img"></div>
+                    <div className="about-img">
+                        <h1 className="about-img-text">NO LIMIT</h1>
+                    </div>
                     <h3 className="about-h3">Medarbetare</h3>
                 <div className="personal-content">
-                    {lang.personal.map((staff, key) =><div className="personal-card" key={key}> 
-                    <img className="image"src={`/images/${staff.img}`} alt={text().staff.alt_text}/> 
+                    {text().personalInfo.map((staff, key) => <div className="personal-card" key={key}> 
+                    <img className="image"src={`/images/${staff.img}`} alt={staff.alt_text}/> 
                         <div className="about-text">
                             <h2>{staff.name}</h2>
                             <h4>{staff.role}</h4>
