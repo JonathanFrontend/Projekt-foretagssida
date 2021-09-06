ee1699fbafa9c18f3eb2911584381263ff679029
Mandus Lindström <mandus85@hotmail.com>
2021-09-06 13:33:31 +0200

    Added css

--- foretagssida-webshop/src/components/News.js
+++ foretagssida-webshop/src/components/News.js
@@ -50,75 +50,76 @@ export default function News() {
             <div className="header-img">
                 <h1 className="header-text">Nyheter</h1>
             </div>
+            <div className="app-container">
+                <div className="newsWrapper">
 
-            <div className="newsWrapper">
+                    <h2 className="newsH2">Våra Nyheter och Artiklar</h2>
 
-                <h2 className="newsH2">Våra Nyheter och Artiklar</h2>
+                    <div className="newsBoxforFilterandSort">
 
-                <div className="newsBoxforFilterandSort">
+                        <div className="newsBoxFilter">
 
-                    <div className="newsBoxFilter">
-
-                        <div className="newsArrow">
-                            <p>Filtrera</p>
-                            <button className="arrow_down" onClick={() => setAddFilter(!addFilter)}>
-                                <img className="arrow_size purp" src={arrowDownPurp} alt="arrow_down" />
-                            </button>
-                        </div>
-                        {addFilter ?
-                            <div className="newsButtonBox">
-                                <aside className="newsAside">
-                                    <button className="newsButton">Nyheter</button>
-                                    <input type="checkbox" checked={checkedOne} onChange={() => setCheckedOne(!checkedOne)} />
-                                </aside>
-
-                                <aside className="newsAside">
-                                    <button className="newsButton">Artiklar</button>
-                                    <input type="checkbox" checked={checkedTwo} onChange={() => setCheckedTwo(!checkedTwo)} />
-                                </aside>
+                            <div className="newsArrow">
+                                <p>Filtrera</p>
+                                <button className="arrow_down" onClick={() => setAddFilter(!addFilter)}>
+                                    <img className="arrow_size purp" src={arrowDownPurp} alt="arrow_down" />
+                                </button>
                             </div>
+                            {addFilter ?
+                                <div className="newsButtonBox">
+                                    <aside className="newsAside">
+                                        <button className="newsButton">Nyheter</button>
+                                        <input type="checkbox" checked={checkedOne} onChange={() => setCheckedOne(!checkedOne)} />
+                                    </aside>
 
-                            : ''}
+                                    <aside className="newsAside">
+                                        <button className="newsButton">Artiklar</button>
+                                        <input type="checkbox" checked={checkedTwo} onChange={() => setCheckedTwo(!checkedTwo)} />
+                                    </aside>
+                                </div>
+
+                                : ''}
+
+                        </div>
+
+                        <div className="newsBoxFilter">
+
+                            <div className="newsArrow">
+                                <p>Sortera</p>
+                                <button className="arrow_down" onClick={() => setAddSort(!addSort)}>
+                                    <img className="arrow_size" src={arrowDownPurp} alt="arrow_down" />
+                                </button>
+                            </div>
+                            {addSort ?
+                                <div className="articleButtonBox">
+                                    <aside className="articleAside">
+                                        <button className="articleButton">Datum - Senaste Först</button>
+                                        <input type="radio" name="sort" defaultChecked onClick={() => setRadioChecked(true)} />
+                                    </aside>
+
+
+                                    <aside className="articleAside">
+                                        <button className="articleButton">Datum - Äldst Först</button>
+                                        <input type="radio" name="sort" onClick={() => setRadioChecked(false)} />
+                                    </aside>
+                                </div>
+                                : ''}
+                        </div>
 
                     </div>
 
-                    <div className="newsBoxFilter">
-
-                        <div className="newsArrow">
-                            <p>Sortera</p>
-                            <button className="arrow_down" onClick={() => setAddSort(!addSort)}>
-                                <img className="arrow_size" src={arrowDownPurp} alt="arrow_down" />
-                            </button>
-                        </div>
-                        {addSort ?
-                            <div className="articleButtonBox">
-                                <aside className="articleAside">
-                                    <button className="articleButton">Datum - Senaste Först</button>
-                                    <input type="radio" name="sort" defaultChecked onClick={() => setRadioChecked(true)} />
-                                </aside>
-
-
-                                <aside className="articleAside">
-                                    <button className="articleButton">Datum - Äldst Först</button>
-                                    <input type="radio" name="sort" onClick={() => setRadioChecked(false)} />
-                                </aside>
+                    <div className="content">
+                        {content.map((data, key) =>
+                            <div className="contentBox" key={key}>
+                                <h6 className="contentHeadline">{data.headline}</h6>
+                                <div className="miniContent">
+                                    <p>{data.date}</p>
+                                    <p>Författare - {data.author}</p>
+                                    <p>Typ -  {data.label}</p>
+                                </div>
                             </div>
-                            : ''}
+                        )}
                     </div>
-
-                </div>
-
-                <div className="content">
-                    {content.map((data, key) =>
-                        <div className="contentBox" key={key}>
-                            <h6 className="contentHeadline">{data.headline}</h6>
-                            <div className="miniContent">
-                                <p>{data.date}</p>
-                                <p>Författare - {data.author}</p>
-                                <p>Typ -  {data.label}</p>
-                            </div>
-                        </div>
-                    )}
                 </div>
             </div>
         </div >
