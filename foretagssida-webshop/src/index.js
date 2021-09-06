725a57384d2ffffb0b4ee8b8dc0e9fe7f1095914
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2021-09-06 09:22:14 +0200

    Trying out I18next

--- foretagssida-webshop/src/index.js
+++ foretagssida-webshop/src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import './i18n';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Suspense fallback={(<div>Loading...</div>)}>
+    <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
 
